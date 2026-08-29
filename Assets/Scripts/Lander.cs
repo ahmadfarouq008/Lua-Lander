85777bf74a82d73bce6647d59a62d895e1d334fa
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-08-30 00:21:08 +0500

    Included left/right rotation with AddTorque(unity feature) and updated values in rigidbody2D options/scaling.

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -11,12 +11,22 @@ public class Lander : MonoBehaviour {
     }
     private void FixedUpdate(){
         if (Keyboard.current.upArrowKey.isPressed ){
-            float force = 700f;
+
+            float force = 700f ;
             LanderRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
         }
-    
-    }
+        if (Keyboard.current.rightArrowKey.isPressed){
 
+            float turnSpeed = -100f ;
+            LanderRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+        if (Keyboard.current.leftArrowKey.isPressed){
+
+            float turnSpeed = +100f ;
+            LanderRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+    }
+    
 }
     
 
