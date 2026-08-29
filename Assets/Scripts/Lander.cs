31fbea0b297ad4693dd46c0cae3d34ff5a2eb3eb
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-08-29 23:36:08 +0500

    added the variable of LanderRiigidbody2D and to refference it as a rigidbody2D component , put it in awake function.

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -3,19 +3,15 @@ using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour {
 
-    private void Update() {
+    private Rigidbody2D LanderRigidbody2D ;
 
-       if (Keyboard.current.upArrowKey.isPressed){
-        Debug.Log("Up.");
-       }
-       if (Keyboard.current.leftArrowKey.isPressed){
-        Debug.Log("Left.");
-       }
-       if (Keyboard.current.rightArrowKey.isPressed){
-        Debug.Log("Right.");
-       }
-       if (Keyboard.current.downArrowKey.isPressed){
-        Debug.Log("Down.");
-       }
-    }
+    private void Awake() {
+        
+        LanderRigidbody2D = GetComponent<Rigidbody2D>() ;
+    }    
+    
 }
+    
+
+
+
