f22f0b74c5f3142f7d4749dd85f14e039614e8ed
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-08-30 00:02:34 +0500

    add upward thrust/force with AddForce (from unity) on UpArrow

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -6,10 +6,17 @@ public class Lander : MonoBehaviour {
     private Rigidbody2D LanderRigidbody2D ;
 
     private void Awake() {
-        
+
         LanderRigidbody2D = GetComponent<Rigidbody2D>() ;
-    }    
+    }
+    private void FixedUpdate(){
+        if (Keyboard.current.upArrowKey.isPressed ){
+            float force = 700f;
+            LanderRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+        }
     
+    }
+
 }
     
 
