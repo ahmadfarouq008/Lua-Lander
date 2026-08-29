e7df9154e71510df8c5208755c9e86d5f2825b16
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-08-29 19:58:45 +0500

    Tested the script of lander by loging start and update and made the functions private ( an access modifier which means no ither script can call it)

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class Lander : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Debug.Log("Start");
+    }
+    
+    private void Update()
+    {
+        Debug.Log("Update");
     }
 }
