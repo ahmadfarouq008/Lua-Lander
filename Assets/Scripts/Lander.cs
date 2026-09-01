18f04b74683b434f2460a2043b3610f4267fa984
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-09-02 02:27:05 +0500

    feat: add hard landing check

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -26,7 +26,16 @@ public class Lander : MonoBehaviour {
             LanderRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
     }
-    
+    private void OnCollisionEnter2D(Collision2D collision){
+        // now in this space od code line, we get the argument input(speed/hit data), when lander falls and this function is invoked automatically by untiy and argument input (that we got in unity) is stored in collision(parameter) as writen in below line "if" code. 
+        float softLandingVelocityMagnitude = 4f ;
+        if (collision.relativeVelocity.magnitude > softLandingVelocityMagnitude ){
+        Debug.Log("Landed too hard!");
+        return ;
+        }       
+        Debug.Log("Landed successfully!");    
+    }
+
 }
     
 
