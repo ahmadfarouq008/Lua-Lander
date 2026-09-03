6b0e6429674a38d0d25394a2cfb38209fdaeb310
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-09-03 22:12:30 +0500

    feat: add custom landing scoring (angle & speed 0-100) with no magic numbers

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -42,21 +44,36 @@ public class Lander : MonoBehaviour {
         }
 
         // now in this space of code line, we get the argument input(speed/hit data), when lander falls and this function is invoked automatically by untiy and argument input (that we got in unity) is stored in collision(parameter) as writen in below line "if" code. 
-        float softLandingVelocityMagnitude = 4f ;
-        float relativeVelocityMagnitude = collision.relativeVelocity.magnitude ;
+        float softLandingVelocityMagnitude = 5f ;                                 //  softLandingVelocityMagnitude means max allowed speed.
+        float relativeVelocityMagnitude = collision.relativeVelocity.magnitude ;  // relativeVeloctyMagnitue means landing speed value.
         if (relativeVelocityMagnitude > softLandingVelocityMagnitude ){
         Debug.Log("Landed too hard!");
         return ;
         }       
         
         float dotVector = Vector2.Dot(Vector2.up,transform.up) ;
-        float minDotVector = .90f ;
+        float minDotVector = .95f ;                                               //  dotVector means landing angle value.
         if (dotVector < minDotVector){
         Debug.Log("Landed on a too steep angle!") ;
         return;
         }
 
         Debug.Log("Landed successfully!");
+
+        // Calculation of score based on landing angle and landing speed.
+        
+        // ANGLE SCORE - how straight you are. (1 = perfect up, 0 = sideways, -1 = upside down)
+        float maxScoreLandingAngle = 100f ;    // 0.97 * 100 = 97
+        float angleScore = Mathf.Clamp01(dotVector) * maxScoreLandingAngle ; // Clamp01 means if dotVector is less than 0(e.g -0.3) then it will become 0 and if it is more than 1(which cant be because straight tip = 1) then it will be 1 .
+
+        // SPEED SCORE - how slow you are.  
+        float maxScoreLandingSpeed = 100f ;   // Speed 2 -> 2/5=0.4 -> 1-0.4=0.6 -> 0.6*100 = 60 score
+        float speedScore = ( 1f - relativeVelocityMagnitude / softLandingVelocityMagnitude ) * maxScoreLandingSpeed ;   // 1f means 100% (Human language ) = 1.0(Computer language) i.e. How much speed is LEFT from 1 (invert) and then we get point(.) somthing value which is then multiplied with 100 to get score in 0-100.(ones to <= hundreds) range.
+
+        // FINAL SCORE - average of angle and speed scores.
+        float finalScore = (angleScore + speedScore) / 2f ;
+
+        Debug.Log( $" Angle Score: {angleScore:F0}/100 | Speed Score: {speedScore:F0}/100 | Final Score: {finalScore:F0}/100" ) ;
     }
 
 }
