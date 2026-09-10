d9ca097b9c9cb194ab92940bd46c8f10b6e320b7
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-09-10 20:47:00 +0500

    feat: add/create LanderVisuals script to handle the thrusters logic according to inputs /controls and reference the ThrusterParticleSystem of each left , middle and right thrusters inside LanderVisuals script.

---
+++ Assets/Scripts/LanderVisuals.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LanderVisuals : MonoBehaviour {
+    [SerializeField] ParticleSystem leftThrusterParticleSystem ;
+    [SerializeField] ParticleSystem middleThrusterParticleSystem ;
+    [SerializeField] ParticleSystem rightThrusterParticleSystem ;
+    
+}
